ea501ddd2993b642afc81e72da7a87998ae04fe5
Neeraj Kumar <neeraj.paruthi087@gmail.com>
2023-01-03 09:06:22 +0530

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,5 +1,5 @@
 def call(){
      sh "docker stop numericApp | true"
      sh "docker rm numericApp | true"
-     sh "docker run --name numericApp -d -p 8085:8080 52.44.217.82:8083/numericapp:${TAG}"
+     sh "docker run --name numericApp -d -p 8085:8080 52.44.217.82:8083/numericapp:v1"
 }
