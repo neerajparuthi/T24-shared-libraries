b0a60258324cae1d99ad85542d631e62f945c82e
Neeraj Kumar <neeraj.paruthi087@gmail.com>
2023-01-03 11:13:26 +0530

    Update Constants.groovy

--- src/cg/t24/Constants.groovy
+++ src/cg/t24/Constants.groovy
@@ -2,6 +2,6 @@ package cg.t24
 
 class Constants {
   static final String TEAMS_MESSAGE = "Sending Notification to TEAMS"
-  # devops internal static final String URL_WEBHOOK = "https://capgemini.webhook.office.com/webhookb2/b3234595-2621-4b83-9311-d463c9cb46c0@76a2ae5a-9f00-4f6b-95ed-5d33d77c4d61/IncomingWebhook/27700534d0d042bd8bb77fde3062a904/946e9597-4fd1-4ab5-b676-777ae9125c3c"
+  // devops internal static final String URL_WEBHOOK = "https://capgemini.webhook.office.com/webhookb2/b3234595-2621-4b83-9311-d463c9cb46c0@76a2ae5a-9f00-4f6b-95ed-5d33d77c4d61/IncomingWebhook/27700534d0d042bd8bb77fde3062a904/946e9597-4fd1-4ab5-b676-777ae9125c3c"
   static final String URL_WEBHOOK = "https://capgemini.webhook.office.com/webhookb2/a07e7adf-c178-4db7-a846-10a1d758bd6c@76a2ae5a-9f00-4f6b-95ed-5d33d77c4d61/IncomingWebhook/0135c7ee861944f4b7d96db33b8ce9e3/946e9597-4fd1-4ab5-b676-777ae9125c3c"
 }
