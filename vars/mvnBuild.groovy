6cc4bdb739ee54d7bf06f40ad8b76c7beecd4c9a
Neeraj Kumar <neeraj.paruthi087@gmail.com>
2023-01-03 07:46:55 +0530

    Create mvnBuild.groovy

---
+++ vars/mvnBuild.groovy
@@ -0,0 +1,20 @@
+def call() {
+      node {
+//        stage("Compile") {
+//             sh "maven-coe clean compile"
+//         }
+
+//         stage("Unit Test") {
+//             sh "maven-coe test"
+//         }
+
+//         stage("Integration Test") {
+//             sh "maven-coe verify"
+//         }
+
+        stage("Package Artifact Jar") {
+            sh "maven coe package -DskipTests=true"
+        }
+       
+    }
+}
