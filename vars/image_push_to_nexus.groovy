46ee51bc275fa82342ada9e5b8a3489961964bea
Neeraj Kumar <neeraj.paruthi087@gmail.com>
2023-01-03 09:40:08 +0530

    Update image_push_to_nexus.groovy

--- vars/image_push_to_nexus.groovy
+++ vars/image_push_to_nexus.groovy
@@ -1,6 +1,6 @@
 def call() {
    docker.withRegistry('http://52.44.217.82:8083', 'nexusrepocreds') {
-                        docker.image("52.44.217.82:8083/numericapp:env.TAG").push()
+      docker.image("52.44.217.82:8083/numericapp:${TAG}").push()
                         //docker.image("docker4028/hello-world:${TAG}").push("latest")
                     }
 }
