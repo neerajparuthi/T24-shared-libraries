0087d54340ac271af0b3ccc3b25622c37c10f7fc
Neeraj Kumar <neeraj.paruthi087@gmail.com>
2023-01-03 08:55:06 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,4 @@
 def call() {
 //docker.build("52.44.217.82:8083/numericapp:${TAG}")
-  sh 'docker build -t 52.44.217.82:8083/numericApp:v1'
+  sh 'docker build -t 52.44.217.82:8083/numericapp:v1 .'
 }
