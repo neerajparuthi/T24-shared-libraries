f6f2d4ccc3415dec9d6b2dd9a9ff143732f89f2b
Neeraj Kumar <neeraj.paruthi087@gmail.com>
2023-01-03 10:42:08 +0530

    Update notify.groovy

--- vars/notify.groovy
+++ vars/notify.groovy
@@ -4,6 +4,9 @@ def call(Map config=[:]) {
   if (config.type == "teams") {
      echo Constants.TEAMS_MESSAGE
      echo config.message
+     office365ConnectorWebhooks([
+            [name: "Office 365", url: Cnstants.URL_WEBHOOK, notifyBackToNormal: true, notifyFailure: true, notifyRepeatedFailure: true, notifySuccess: true, notifyAborted: true]
+        ])
   } else {
       echo Constants.TEAMS_MESSAGE
       echo config.message
